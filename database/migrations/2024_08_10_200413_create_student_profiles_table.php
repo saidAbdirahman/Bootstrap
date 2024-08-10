2171f48b1745ba7d30862d16bda31f776d4a5c97
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-10 22:34:40 +0200

    created two tables
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- database/migrations/2024_08_10_200413_create_student_profiles_table.php
+++ database/migrations/2024_08_10_200413_create_student_profiles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('student_profiles', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('student_id');
+            $table->string('country');
             $table->timestamps();
         });
     }
